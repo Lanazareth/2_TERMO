ac1a601c630974bab72c2e06758fb0bb7c3511ed
Lanazareth <lauragoncalvesnazareth@gmail.com>
2026-09-01 09:25:39 -0300

    01/09/26

--- BACKEND/AULA08/ex3.js
+++
@@ -1,4 +0,0 @@
-for(let i = 10; i>= 0 ; i-=1) {
-    console.log(`lançando em... ${i}`);
-}
-console.log('Lançando foguete!');
--- BACKEND/backend1_aula_autonoma/aula03/desafio2aula3.js
+++
@@ -1,7 +0,0 @@
-const produto = "caderno";
-const preco = 15;
-const quantidade = 3;
-const total = preco * quantidade 
-
-console.log("Produto:",produto);
-console.log("valor total: ", total);
