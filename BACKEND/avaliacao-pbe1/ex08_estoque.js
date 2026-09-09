8790bdab1acd4d65c21b06f3fd5c5cc0d093da01
Lanazareth <lauragoncalvesnazareth@gmail.com>
2026-09-09 16:50:55 -0300

    somativa

--- BACKEND/avaliacao-pbe1/ex08_estoque.js
+++ BACKEND/avaliacao-pbe1/ex08_estoque.js
@@ -4,26 +4,11 @@ console.log("=== Estoque de Componentes ===");
 
 const componentes = [];
 
-const quantidadeComponentes = 3;
-const nomesComponentes = ["lidia", "Bruno", "Celso"];
+const quantidadeComponentes = 3
+
 
 for (let i = 0; i < quantidadeComponentes; i++) {
-    const nomeComponente = nomesComponentes[i];
-    const quantidade = entrada.questionInt(`Digite a quantidade do componente ${nomeComponente}: `);
-    const estoqueMinimo = entrada.questionInt(`Digite o estoque mínimo do componente ${nomeComponente}: `);
-
-    const componente = {
-        nome: nomeComponente,
-        quantidade: quantidade,
-        estoqueMinimo: estoqueMinimo
-    };
-
-    componentes.push(componente);
-}
-
-console.log("\n=== Componentes com Estoque Abaixo do Mínimo ===");
-for (const componente of componentes) {
-    if (componente.quantidade < componente.estoqueMinimo) {
-        console.log(`Componente: ${componente.nome} - Quantidade: ${componente.quantidade} - Estoque Mínimo: ${componente.estoqueMinimo}`);
-    }
-}
+    const nomeComponente = entrada.question(`Digite o nome do componente ${i + 1}: `);
+    const quantidadeComponente = entrada.questionInt(`Digite a quantidade em estoque do componente ${i + 1}: `);
+    componentes.push({ nome: nomeComponente, quantidade: quantidadeComponente });
+}   
